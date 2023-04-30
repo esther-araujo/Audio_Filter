506f43e410d74a4a433fe9211293c0e5c3fce08c
EstherWI <esthersss09@gmail.com>
2023-04-30 19:55:48 -0300

    profile on example

--- read.m
+++ read.m
@@ -1,3 +1,8 @@
+% A função profile permite monitorar o desempenho do código, gerando um relatorio
+% sobre o tempo de execução de cada linha e da quantidade de vezes que cada linha é executada
+% Também mostra o tempo de execução do programa geral
+profile on;
+
 % audioread: lê arquivo e retorna os dados de audio 
 % e a frequencia de amostragem do sinal (em Hz)
 % No caso do arquivo de entrada, Fs = 44,1 KHz
@@ -7,5 +12,10 @@
 % correspondentes aos quadros de audio e colunas correspondentes aos canais
 
 % play do audio
-sound(y,Fs);
+%sound(y,Fs);
 
+profile off;
+profile resume;
+
+T = profile ("info");
+profshow (T);
