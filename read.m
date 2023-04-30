74400afa257ababbbb5c113042fdd118dea6abeb
EstherWI <esthersss09@gmail.com>
2023-04-30 16:08:15 -0300

    add read audiofile

---
+++ read.m
@@ -0,0 +1,11 @@
+% audioread: lê arquivo e retorna os dados de audio 
+% e a frequencia de amostragem do sinal (em Hz)
+% No caso do arquiovo de entrada, Fs = 44,1 KHz
+[y,Fs] = audioread('SinalRuidoso.wav');
+
+% os dados do audio (y) são armazenados como uma matriz com linhas
+% correspondentes aos quadros de audio e colunas correspondentes aos canais
+
+% play do audio
+sound(y,Fs);
+
